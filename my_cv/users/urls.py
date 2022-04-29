d9a0853dfb46cb6139585aff84099029c63d01f9
aliaksandr_tsiustsin <aliaksandr_tsiustsin@epam.com>
2022-04-29 13:35:34 +0300

    Fixed password reset success_urls

--- my_cv/users/urls.py
+++ my_cv/users/urls.py
@@ -1,7 +1,39 @@
-from django.urls import path, include
+from django.contrib.auth import views as auth_view
+from django.urls import path
+from django.urls import reverse_lazy
 
 app_name = 'users'
 
+
+class UsersPasswordResetView(auth_view.PasswordResetView):
+    success_url = reverse_lazy("users:password_reset_done")
+
+
+class UsersPasswordResetConfirmView(auth_view.PasswordResetConfirmView):
+    success_url = reverse_lazy("users:password_reset_complete")
+
+
 urlpatterns = [
-    path('', include('django.contrib.auth.urls'))
+    path("login/", auth_view.LoginView.as_view(), name="login"),
+    path("logout/", auth_view.LogoutView.as_view(), name="logout"),
+
+    path(
+        "password_reset/done/",
+        auth_view.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+
+    path("password_reset/", UsersPasswordResetView.as_view(), name="password_reset"),
+
+    path(
+        "reset/<uidb64>/<token>/",
+        UsersPasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+
+    path(
+        "reset/done/",
+        auth_view.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
 ]
